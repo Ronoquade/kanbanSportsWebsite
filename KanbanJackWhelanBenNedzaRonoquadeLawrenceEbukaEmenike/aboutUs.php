658d9882a9bbef301e3104b95e6039562fa24f30
Jw121065 <jwhelan1@ggc.edu>
2025-11-17 11:04:12 -0500

    Update aboutUs.php

---
+++ KanbanJackWhelanBenNedzaRonoquadeLawrenceEbukaEmenike/aboutUs.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pacifico">
+    <style>
+        .w3-pacifico { font-family: "Pacifico", serif; }
+        h1 { font-family: "Pacifico", serif; }
+    </style>
+</head>
+<body>
+    <div class="container w3-theme-d5">
+        <header class="w3-container w3-center w3-blue-gray">
+            <h1>Sports Apparel</h1>
+            <h2>About Us</h2>
+        </header>
+
+        <?php include 'mainMenu.php'; ?>
+
+        <div class="w3-container w3-center">
+            <div class="w3-card-4" style="width:100%">
+                <img src="SportsApparel.png" width="20%" height="22%"
+                class="w3-display-topright">
+                <div class="w3-container w3-center w3-theme-d3">
+                    <h1>Sports Apparel</h1>
+                    <p>By: Ebuka Emenike, Ronoquade Lawrence, Jack Whelan, Ben Nedza</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
